e642e558f062e4304f17548517cdaabf74c59040
FrancescoDale <fd060862@gmail.com>
2020-11-06 14:46:51 +0100

    aggiunta l'immagine + fix : l'immagine non veniva visualizzata a causa di un typo  quando ho usato v-bind

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var app = new Vue (
         el : '#root',
         data : {
             addedText :' Vue Hello!',
+            picture : 'https://www.tavolartegusto.it/wp/wp-content/uploads/2020/03/Carbonara-Spaghetti-alla-carbonara-Ricetta-Carbonara-1280x720.jpg',
         },
 
     }
